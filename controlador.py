03df1a8ccb72d3ceabf74fc61ae56642acc73091
gdurdaneta <gerardo.urdaneta@chiper.co>
2021-05-06 21:43:53 -0300

    up

--- controlador.py
+++ controlador.py
@@ -1,10 +1,18 @@
-from Main_Windows_login import mainlogin
-from logicapy import validauser
-from BD_models import buscarusuario
-from BD_models import buscarpassword
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+import sys
 
-class utnintegrador:
+from Main_Windows_logins import mainlogin
+from Main_Windows_base import mainbase
+"from logicapy import validauser"
+"from BD_models import buscarusuario"
+"from BD_models import buscarpassword"
+
+class integrador(QtWidgets.QMainWindow):
     def __init__(self):
+        super(integrador, self).__init__()
+        self.ui = mainlogin()
+        self.ui.setupUi(self)
 
     def validador(self):
         if buscarusuario == Entry_Usuario:
@@ -15,3 +23,8 @@ class utnintegrador:
         else:
             print("Usuario Incorrecto")
             
+
+app = QtWidgets.QApplication([])
+aplicacion = integrador()
+aplicacion.show()
+sys.exit(app.exec())
